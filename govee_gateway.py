23ef9758b2fc04b3046f586bb21913a3225c735b
jim <jim.mellor@me.com>
2024-09-21 19:00:00 +0100

    further bugfixing bugfies

--- govee_gateway.py
+++ govee_gateway.py
@@ -142,7 +142,7 @@ class ScanDelegate(DefaultDelegate):
 
             
             try:
-                if 'GVH5075' not in adv_list[0]:
+                if 'GVH5075' not in adv_list[0][0]:
                     logging.info("Not a Govee Hygrometer")
                     return
                 else:
